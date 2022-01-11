1833800fb18b51e27e129c972bdcbaa87ca95560
Brennan McCreary <brennanmccreary@gmail.com>
2022-01-11 18:52:50 -0500

    Create react app and establish file structure.

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                Hi
+            </div>
+        )
+    }
+}
+
+export default App;
