0785e78e584554d35dc70aaabb63e4465516ad70
Brennan McCreary <brennanmccreary@gmail.com>
2022-01-11 21:16:44 -0500

    Add styling for DataMapper. Convert DataMapper list to table formatting.

--- src/components/DataMapper/DataMapper.jsx
+++ src/components/DataMapper/DataMapper.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import './DataMapper.css';
 
 
 const DataMapper = (props) => {
     console.log(props.songs.map((el) => el.title));
     return(
-        <div>
-            <h1>
-                {(props.songs.length > 0) ? <ul>{props.songs.map((el, i) => 
-                <li key={i}>
-                    "{el.title}" By: {el.artist} on {el.album}
-                </li>)}</ul> : <div>Nothing</div>} 
+        <div className='container'>
+            <h1 className='song-list col-xs-12'>
+                <table className='song-table'>
+                    <tr>
+                        <th className='table-header id'>ID</th>
+                        <th className='table-header title'>Title</th>
+                        <th className='table-header artist'>Artist</th>
+                        <th className='table-header album'>Album</th>
+                        <th className='table-header'>Genre</th>
+                        <th className='table-header'>Released</th>
+                    </tr>
+                    {(props.songs.length > 0) ? <React.Fragment>{props.songs.map((el, i) => 
+                    <tr key={i}>
+                        <td>{el.id}</td>
+                        <td>"{el.title}"</td> 
+                        <td>{el.artist}</td>
+                        <td>{el.album}</td>  
+                        <td>{el.genre}</td>
+                        <td>{el.releaseDate}</td>
+                    </tr>)}</React.Fragment> : null} 
+                </table>
             </h1>  
         </div>
     );
